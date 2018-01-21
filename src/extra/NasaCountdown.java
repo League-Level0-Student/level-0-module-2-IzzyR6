21980ddcf98d51039502352f1f9097f187b5fcee
cheetah676 <league@WTS22.attlocal.net>
2018-01-20 16:04:53 -0800

    nasa countdown, starshow

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,35 +3,41 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
-  * Somebody has to do the countdown – or the rocket won’t launch.
- * 
- * The ground control chief will tell you where to start – then count down to 0. 
- * 
- * (example for ’11’: 11 10 9 8 7 6 5 4 3 2 1 0)
- * 
- **/
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
+ * Somebody has to do the countdown – or the rocket won’t launch.
+* 
+* The ground control chief will tell you where to start – then count down to 0. 
+* 
+* (example for ’11’: 11 10 9 8 7 6 5 4 3 2 1 0)
+* 
+**/
 
 public class NasaCountdown {
 	public static void main(String[] args) {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+		String number = JOptionPane.showInputDialog("Where do you want to start counting from?");
+		int start = Integer.parseInt(number);
 		// 3. Change the countdown to use the new starting point
-		
-		// 1. Print a countdown from 10 to 0 on the console
 
+		// 1. Print a countdown from 10 to 0 on the console
+		for (int i = start; i >= 0; i--) {
+
+			System.out.println(i);
+			speak(i + "");
+		}
 		// 4. Use the speak method to hear the countdown.
-		
+
 		// 5. when the counting is done, speak "blastoff!"
+		speak("blastoff!");
 	}
 
-static void speak(String words) {
-   	 try {
-   		 Runtime.getRuntime().exec("say " + words).waitFor();
-   	 } catch (Exception e) {
-   		 e.printStackTrace();
-   	 }
-  }
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
--- src/extra/StarShow.java
+++ src/extra/StarShow.java
@@ -3,64 +3,66 @@ package extra;
 import org.jointheleague.graphical.robot.Robot;
 
 /*** Teacher's note ***/
-/* Before beginning recipe: 
-* 1. ask students to find and explain the method in this recipe. 
-* 2. ask students how they might use the method to make the picture in the laminated hand-outs. */
+/*
+ * Before beginning recipe: 1. ask students to find and explain the method in
+ * this recipe. 2. ask students how they might use the method to make the
+ * picture in the laminated hand-outs.
+ */
 
 public class StarShow {
-	
-	Robot robot = new Robot("batman");
-	
+
+	Robot Sophia = new Robot("batman");
+
 	void makeStars() {
-		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+
 		// 13. Set the speed to 8
-
+		Sophia.setSpeed(8);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+		int xposition = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+		int yposition = 950;
 		// 8. Make a variable to hold the star size and set it to 25
-
+		int starsize = 25;
 		// 12. Repeat the steps #19 to #18, 30 times
+		for (int i = 0; i < 30; i++) {
+			Sophia.setRandomPenColor();
+			Sophia.setPenWidth(i);
 
-			// 19. Set the pen width to i 
-	
+			// 19. Set the pen width to i
+
 			// 10. Set the X position of the robot to your X variable
-	
+			Sophia.setX(xposition);
 			// 11. Set the Y position of the robot to your Y variable
-	
+			Sophia.setY(yposition);
 			// 9. Call the drawStar() method with your star size variable
-	
+			drawStar(starsize);
 			// 14. Increase the X position by star size. See Figure 2.
-	
+			xposition = xposition + starsize;
 			// 15. decrease the Y position by star size. See Figure 3.
-	
+			yposition = yposition - starsize;
 			// 16. Increase the star size by 20
-	
+			starsize = starsize + 20;
 			// 17. Turn the robot 12 degrees
-	
-			// 18. Make each star a different random color like in Figure 4. 
+			Sophia.turn(12);
+			// 18. Make each star a different random color like in Figure 4.
 
+		}
 	}
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+		Sophia.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
+		for (int i = 0; i < 5; i++) {
 
 			// 1. Move the robot the distance of the starSize variable
-	
+			Sophia.move(starSize);
 			// 3. Turn the robot 144 degrees
-			
+			Sophia.turn(144);
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		new StarShow().makeStars();
 	}
 }
-
-
-
-
-
